04921b28d59e1e480ed069509a97e60f40ef0290
xTrebliG <gil@crankshiftnyc.com>
2015-06-20 06:39:36 -0400

    Added .sass compiler to css

--- lib/compile.sh
+++ lib/compile.sh
@@ -1,18 +1,33 @@
 echo "$(date) - Compilation begun..."
 #COMPILE HAML FILE
 	COMPILED_HAML="compiled"
-	files=`find app/views -name '*.haml'`
+	HAML_FILES=`find app/views -name '*.haml'`
 		#LOOPS THOUGH ALL FILES WITH .HAML 
-			for i in $files;
+			for i in $HAML_FILES;
 			do
 			  search=$i
 			  html=".html"
 			  #DUPLICATES FILE NAME WITH .HTML 
 			  haml_name="${search/.haml/$html}"
 			  #CREATES OUTPUT PATH
-			  OUTPUT="${COMPILED_HAML}/${haml_name##*/}"
+			  HAML_OUTPUT="${COMPILED_HAML}/${haml_name##*/}"
 			  #COMPILES HAML FILES TO HTML FILES	
-			  haml $i $OUTPUT
+			  haml $i $HAML_OUTPUT
+			done
+#COMPILE SASS FILE
+	COMPILED_SASS="compiled/styles"
+	SASS_FILES=`find app/styles -name '*.sass'`
+	#LOOPS THOUGH ALL FILES WITH .SASS 
+			for x in $SASS_FILES;
+			do
+			  search=$x
+			  css=".css"
+			  #DUPLICATES FILE NAME WITH .HTML 
+			  sass_name="${search/.sass/$css}"
+			  #CREATES OUTPUT PATH
+			  SASS_OUTPUT="${COMPILED_SASS}/${sass_name##*/}"
+			  #COMPILES HAML FILES TO HTML FILES	
+			  sass $x:$SASS_OUTPUT
 			done
 echo "$(date) - Compilation Complete!"
 echo "Watching App For Changes..."
